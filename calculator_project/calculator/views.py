82cec51acccc0b97857fe9be9482ba9a3ecb1bb8
vishnupriyan <priyanvishnu15@gmail.com>
2020-05-14 15:56:22 +0530

    first commit

---
+++ calculator_project/calculator/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import math
+from django.http import HttpResponse,JsonResponse
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydictionary = {
+            'q' : q,
+            'ans' : ans,
+            'error' : False,
+            'result' : True
+        }
+        return render(request,'index.html',context=mydictionary)
+    except:
+        mydictionary = {
+            "error" : True,
+            "result": False
+        }
+        return render(request,'index.html',context=mydictionary)
